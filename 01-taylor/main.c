9f4a38f5a364e331a2db6ebaf153df813379892f
sgrams <sjg@fmdx.pl>
2018-10-13 16:17:33 +0200

    [01-taylor] add main.c file

---
+++ 01-taylor/main.c
@@ -0,0 +1,18 @@
+/*
+ *  This file is part of 01-taylor task.
+ * 
+ *  main.c
+ *  Stanisław Grams
+ */
+#include <stdint.h>
+/*
+ * Entrypoint of the program
+ *
+ * @arg    int32_t  argc
+ * @arg    int8_t **argv
+ * @retval int32_t
+ *
+ */
+int32_t main (int32_t argc, char **argv) {
+  return 0;
+}
