a9ec148aa1bc0f8d3fac4c80dabd6f02104de9ba
sgrams <sjg@fmdx.pl>
2018-10-14 11:45:18 +0200

    [01-taylor] work in progress (wip) (wip)

--- 01-taylor/main.c
+++ 01-taylor/main.c
@@ -9,6 +9,11 @@
 #include <quadmath.h>
 #include <pthread.h>
 #include "taylor.h"
+#include "util.h"
+
+#define  MAX_RESULTS 6
+#define  MAX_THREADS MAX_RESULTS
+#define  OUTPUT_FILEPATH "output.csv"
 
 /*
  * Calculate and write to buffer
@@ -17,6 +22,67 @@
  * 
  * @arg    __float128 *buffer
  * @retval     gint32
+ *
+ */
+void * worker_func0 (void *buffer) {
+      gint32  i;
+  __float128  iter;
+  __float128 *array = buffer;
+
+  for (i = 0, iter = INTERVAL_MIN; i < MAX_ELEMENTS; iter += Q_SUM_STEP, i++)
+  {
+    array[i] = taylor_func0 (iter);
+  }
+  buffer = array;
+
+  pthread_exit (NULL);
+}
+
+/*
+ * Calculate and write to buffer
+ * first function (taylor_func1)
+ * of a given number of elements
+ * 
+ * @arg    __float128 *buffer
+ * @retval     gint32
+ *
+ */
+void * worker_func1 (void *buffer) {
+      gint32  i;
+  __float128  iter;
+  __float128 *array = buffer;
+
+  for (i = 0, iter = INTERVAL_MIN; i < MAX_ELEMENTS; iter += Q_SUM_STEP, i++)
+  {
+    array[i] = taylor_func1 (iter);
+  }
+  buffer = array;
+
+  pthread_exit (NULL);
+}
+
+/*
+ * Calculate and write to buffer
+ * first function (taylor_func2)
+ * of a given number of elements
+ * 
+ * @arg    __float128 *buffer
+ * @retval     gint32
+ *
+ */
+void * worker_func2 (void *buffer) {
+      gint32  i;
+  __float128  iter;
+  __float128 *array = buffer;
+
+  for (i = 0, iter = INTERVAL_MIN; i < MAX_ELEMENTS; iter += Q_SUM_STEP, i++)
+  {
+    array[i] = taylor_func2 (iter);
+  }
+  buffer = array;
+
+  pthread_exit (NULL);
+}
 
 /*
  * Entrypoint of the program
@@ -27,25 +93,57 @@
  *
  */
 gint32 main (gint32 argc, gchar **argv) {
-  gint32      i;
-//gchar      *str;
-  __float128 *array;
-//__float128  numb;
-  __float128  iter;
+  gint32       i;
+  gint32       ret;
+  pthread_t    threads[MAX_THREADS];
+  __float128  **array;
+  FILE        *output_file;
 
-/*
-  numb = taylor_func0 (5.00000000000000000000000000000001Q);
-  str = taylor_print (numb);
-  printf ("%s\n", str);
-  free (str);
-*/
-  array = g_malloc0 (30000000 * sizeof (__float128));
-  for (i = 0, iter = INTERVAL_MIN; iter < INTERVAL_MAX-Q_SUM_STEP; iter += Q_SUM_STEP, i++)
+  array = g_malloc (MAX_RESULTS * sizeof (__float128 *));
+  for (i = 0; i < MAX_RESULTS; i++)
   {
-    array[i] = taylor_func0 (iter);
+    array[i] = g_malloc (MAX_ELEMENTS * sizeof (__float128));
+  }
+  // Create thread for worker_func0
+  ret = pthread_create (&threads[0], NULL, worker_func0, (void *)array[0]);
+  if (ret) {
+    fprintf (stdout, "Unable to create thread for worker_func0: %i\n", ret);
+    exit (-1);
   }
 
-  g_free (array);
+  // Create thread for worker_func1
+  ret = pthread_create (&threads[1], NULL, worker_func1, (void *)array[1]);
+  if (ret) {
+    fprintf (stdout, "Unable to create thread for worker_func1: %i\n", ret);
+    exit (-1);
+  }
 
-  return 0;
+  // Create thread for worker_func2
+  ret = pthread_create (&threads[2], NULL, worker_func2, (void *)array[2]);
+  if (ret) {
+    fprintf (stdout, "Unable to create thread for worker_func2: %i\n", ret);
+    exit (-1);
+  }
+
+  // Wait till all threads join main program
+  pthread_join (threads[0], NULL);
+  pthread_join (threads[1], NULL);
+  pthread_join (threads[2], NULL);
+
+  // Save calculated data to a comma-separated file
+  output_file = fopen (OUTPUT_FILEPATH, "w+");
+  if (output_file == NULL) {
+    printf ("Unable to open file! Exiting...\n");
+    return 1;
+  }
+
+  util_save_array (output_file, array);
+
+  // Free all of array elements
+  for (i = 0; i < MAX_RESULTS; i++) {
+    g_free (array[i]);
+  }
+  fclose (output_file);
+
+  pthread_exit (NULL);
 }
--- 01-taylor/taylor.h
+++ 01-taylor/taylor.h
@@ -12,19 +12,19 @@
 // Defining macros
 #define      MAX_BUF 1024
 
-#define   Q_MAX_PREC 0.00000000000000000000000000000001Q
 #define STR_MAX_PREC "64"
 
-#define   Q_EXP_STEP 192
-#define   Q_SIN_STEP 192
+#define   Q_EXP_STEP 16
+#define   Q_SIN_STEP 16
 #define   Q_SUM_STEP 0.000001Q
 
-#define INTERVAL_MIN -15.0Q
-#define INTERVAL_MAX  15.0Q
+#define INTERVAL_MIN -1.2Q
+#define INTERVAL_MAX 1.2Q
+#define MAX_ELEMENTS 2400000
 
 // Defining basic functions to operate on 128 bit
 // floating-point precision numbers
-char *      taylor_print (__float128 numb);
+char       *taylor_print (__float128 numb);
 
 // Redefinition of math functions using basic
 // mathematical operations.
