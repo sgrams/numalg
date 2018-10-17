b4298f35788334bb54017d6d4fc03687427a0605
sgrams <sjg@fmdx.pl>
2018-10-17 21:45:27 +0200

    [01-taylor] final version 1.0

--- 01-taylor/scripts/00_plot_with_octave_m008.m
+++ 01-taylor/scripts/00_plot_with_octave_m008.m
@@ -14,7 +14,7 @@ ylabel ("exp(sin(x))");
 xlabel ("arguments");
 title  ("Plot of exp(sin(x)), M=8");
 legend ("func0", "func1", "func2", "func3", "func4");
-print -dpng 01_functions_m008.png;
+print -dsvg 01_functions_m008.svg;
 
 ### diffs
 plot (output_m8(:,1), abs (output_m8(:,2) - output_m8(:,3)));
@@ -22,42 +22,25 @@ ylabel ("abs (func0(x) - func1(x))");
 xlabel ("arguments");
 title  ("abs (func0(x) - func1(x)), M=8");
 legend ("abs (func0(x) - func1(x))");
-print -dpng 02_plot_diff_func0-func1_m008.png;
+print -dsvg 02_plot_diff_func0-func1_m008.svg;
 
 plot (output_m8(:,1), abs (output_m8(:,2) - output_m8(:,4)));
 ylabel ("abs (func0(x) - func2(x))");
 xlabel ("arguments");
 title  ("abs (func0(x) - func2(x)), M=8");
 legend ("abs (func0(x) - func2(x))");
-print -dpng 03_plot_diff_func0-func2_m008.png;
+print -dsvg 03_plot_diff_func0-func2_m008.svg;
 
 plot (output_m8(:,1), abs (output_m8(:,2) - output_m8(:,5)));
 ylabel ("abs (func0(x) - func3(x))");
 xlabel ("arguments");
 title  ("abs (func0(x) - func3(x)), M=8");
 legend ("abs (func0(x) - func3(x))");
-print -dpng 04_plot_diff_func0-func3_m008.png;
+print -dsvg 04_plot_diff_func0-func3_m008.svg;
 
 plot (output_m8(:,1), abs (output_m8(:,2) - output_m8(:,6)));
 ylabel ("abs (func0(x) - func4(x))");
 xlabel ("arguments");
 title  ("abs (func0(x) - func4(x)), M=8");
 legend ("abs (func0(x) - func4(x))");
-print -dpng 05_plot_diff_func0-func4_m008.png;
-
-
-### diffs 2
-plot (output_m8(:,1), abs (output_m8(:,3) - output_m8(:,4)));
-ylabel ("abs (func1(x) - func2(x))");
-xlabel ("arguments");
-title  ("abs (func1(x) - func2(x)), M=8");
-legend ("abs (func1(x) - func2(x))");
-print -dpng 06_plot_diff_func1-func2_m008.png;
-
-
-plot (output_m8(:,1), abs (output_m8(:,5) - output_m8(:,6)));
-ylabel ("abs (func3(x) - func4(x))");
-xlabel ("arguments");
-title  ("abs (func3(x) - func4(x)), M=8");
-legend ("abs (func3(x) - func4(x))");
-print -dpng 07_plot_diff_func3-func4_m008.png;
+print -dsvg 05_plot_diff_func0-func4_m008.svg;
