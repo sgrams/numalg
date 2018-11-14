65884714b04c73a79881c10ca5f1384e7b498b25
Jezierski, Maciej <maciejjezierski4@gmail.com>
2018-11-14 23:28:06 +0100

    added shit to print output of basic gauss

--- 02-gauss/src/main.cc
+++ 02-gauss/src/main.cc
@@ -7,11 +7,13 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-  MyMatrix<float>*matrix = new MyMatrix<float>(10);
   
+  MyMatrix<float>* matrix = new MyMatrix<float>(3);
   matrix->fill();
   matrix->print();
-  float* Tab = matrix->backsub();
+  float *tab;
+  tab=matrix->gaussian_no_pivoting();
+  for (int i=0; i<3; i++) cout<<tab[i]<<endl;
 
   delete[] Tab;
   delete matrix;
