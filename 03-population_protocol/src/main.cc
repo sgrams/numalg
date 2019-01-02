2cd5fb56f3609bc2c4e4c0eae3edf9112d241ace
Grams, Stanisław sgrams@fmdx.pl <sjg@fmdx.pl>
2019-01-02 18:40:00 +0100

    [03-population_protocols] new project structure

---
+++ 03-population_protocol/src/main.cc
@@ -0,0 +1,15 @@
+/*
+ * This file is part of 03-population_protocol task.
+ *
+ * src/main.cc
+ * Stanisław Grams <sgrams@fmdx.pl>
+ */
+#include <cstdlib>
+#include "util.hh"
+#include "protocols.hh"
+
+int main (int argc, char *argv[])
+{
+
+  return EXIT_SUCCESS;
+}
---
+++ 03-population_protocol/src/protocols.hh
@@ -0,0 +1,15 @@
+/*
+ * This file is part of 03-population_protocol task.
+ *
+ * src/main.cc
+ * Stanisław Grams <sgrams@fmdx.pl>
+ */
+#include <cstdlib>
+
+typedef enum state {
+  Y,
+  N,
+  U
+} state_t;
+
+
