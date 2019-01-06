2927567f3c045c927ec7f8f592cdbf58baa81dd0
Stanisław Grams <sgrams@fmdx.pl>
2019-01-06 13:55:01 +0100

    [03-population_protocols] new class named Generator

---
+++ 03-population_protocols/src/generator.hh
@@ -0,0 +1,23 @@
+/*
+ * This file is part of 03-population_protocol task.
+ *
+ * src/protocol.hh
+ * Stanisław Grams <sgrams@fmdx.pl>
+ */
+#ifndef PROTOCOLS_GENERATOR_HH
+#define PROTOCOLS_GENERATOR_HH
+#include <cstdlib>
+#include <random>
+#include <ctime>
+#include <algorithm>
+#include <iostream>
+#include "util.hh"
+#include "gauss.hh"
+
+class
+Generator {
+  private:
+  public:
+};
+
+#endif // PROTOCOLS_GENERATOR_HH
