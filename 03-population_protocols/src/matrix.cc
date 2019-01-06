add750b421d848f7d7ea83b7b88a51ea02f4737d
Stanisław Grams <sgrams@fmdx.pl>
2019-01-06 13:53:07 +0100

    [03-population_protocols] renamed gauss.cc to matrix.cc

--- 03-population_protocols/src/matrix.cc
+++ 03-population_protocols/src/matrix.cc
@@ -1,16 +1,9 @@
 /*
  *  This file is part of 03-population_protocols task.
- * 
- *  src/gauss.cc
+ *  
+ *  src/matrix.cc
  *  Stanislaw Grams <sgrams@fmdx.pl>
  *  Maciej Jezierski <maciejjezierski4@gmail.com>
  *  Juliusz Korczakowski <iuliuszkor@gmail.com>
  */
-#include "gauss.hh"
-
-// overloading i/o
-ostream& operator << (ostream &os, const MyType &mt)
-{
-  os << fixed << mt.value;
-  return os;
-}
+#include "matrix.hh"
