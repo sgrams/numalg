4195e3546caac504a244267e1591ebd1b0cea2bd
Stanisław Grams <sgrams@fmdx.pl>
2019-01-11 12:27:37 +0100

    [04-approximations] initial commit

---
+++ 04-approximations/src/main.cc
@@ -0,0 +1,22 @@
+/*
+ * This file is part of 04-approximations task.
+ *
+ * src/main.cc
+ * Stanisław Grams <sgrams@fmdx.pl>
+ */
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+#include "util.hh"
+#include "protocol.hh"
+#include "matrix.hh"
+#include "generator.hh"
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+
+  return EXIT_SUCESS;
+}
