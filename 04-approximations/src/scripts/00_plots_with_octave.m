02a9f5a846624b8ad79f483a506b9af222df9f6c
Juliusz <gaunt1111@gmail.com>
2019-01-13 23:23:22 +0100

    script extended

--- 04-approximations/src/scripts/00_plots_with_octave.m
+++ 04-approximations/src/scripts/00_plots_with_octave.m
@@ -22,8 +22,8 @@ all_time = [(gauss_time), (gauss_sparse_time), (seidel_1e10_time), (seidel_eigen
 plot(matrix_size, all_time, "linewidth", 4);
 ylabel ("time");
 xlabel ("matrix size");
-title ("Calculation time execution");
-legend ("Gauss", "Gauss-Sparse", "Gauss-Seidel 1e-10", "Gauss-Seidel-Sparse 1e-10", "SparseLU", "location", "northwest");
+title ("calculation time execution");
+legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
 
 print -dpng ../../report/plots/01_calc_time_execution_all_methods.png;
 
@@ -38,8 +38,8 @@ gen_all_time = [(gen_gauss_time), (gen_gauss_sparse_time), (gen_seidel_1e10_time
 plot(matrix_size, gen_all_time, "linewidth", 4);
 ylabel ("time");
 xlabel ("matrix size");
-title ("Generate time execution");
-legend ("Gauss", "Gauss-Sparse", "Gauss-Seidel 1e-10", "Gauss-Seidel-Sparse 1e-10", "SparseLU", "location", "northwest");
+title ("generate time execution");
+legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
 
 print -dpng ../../report/plots/02_gen_time_execution_all_methods.png;
 
@@ -49,13 +49,13 @@ gauss_sparse_abs = gauss_sparse(3:60,4);
 seidel_1e10_abs = gs_1e10(3:60,4);
 seidel_eigen_abs = gs_eigen(3:60,4);
 lu_eigen_abs = lu_eigen(3:60,4);
-all_time = [(gauss_abs), (gauss_sparse_abs), (seidel_1e10_abs), (seidel_eigen_abs), (lu_eigen_abs)];
+all_abs = [(gauss_abs), (gauss_sparse_abs), (seidel_1e10_abs), (seidel_eigen_abs), (lu_eigen_abs)];
 
-plot(matrix_size, all_time, "linewidth", 4);
+plot(matrix_size, all_abs, "linewidth", 4);
 ylabel ("abs error");
 xlabel ("matrix size");
-title ("Calculation abs error");
-legend ("Gauss", "Gauss-Sparse", "Gauss-Seidel 1e-10", "Gauss-Seidel-Sparse 1e-10", "SparseLU", "location", "northwest");
+title ("calculation abs error");
+legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
 
 print -dpng ../../report/plots/03_calc_abs_error_all_methods.png;
 
@@ -65,16 +65,47 @@ gauss_sparse_rel = gauss_sparse(3:60,5);
 seidel_1e10_rel = gs_1e10(3:60,5);
 seidel_eigen_rel = gs_eigen(3:60,5);
 lu_eigen_rel = lu_eigen(3:60,5);
-all_time = [(gauss_rel), (gauss_sparse_rel), (seidel_1e10_rel), (seidel_eigen_rel), (lu_eigen_rel)];
+all_rel = [(gauss_rel), (gauss_sparse_rel), (seidel_1e10_rel), (seidel_eigen_rel), (lu_eigen_rel)];
 
-plot(matrix_size, all_time, "linewidth", 4);
+plot(matrix_size, all_rel, "linewidth", 4);
 ylabel ("rel error");
 xlabel ("matrix size");
-title ("Calculation rel error");
-legend ("Gauss", "Gauss-Sparse", "Gauss-Seidel 1e-10", "Gauss-Seidel-Sparse 1e-10", "SparseLU", "location", "northeast");
+title ("calculation rel error");
+legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
 
 print -dpng ../../report/plots/04_calc_rel_error_all_methods.png;
 
+###gen all methods abs error
+gen_gauss_abs = gauss(3:60,8);
+gen_gauss_sparse_abs = gauss_sparse(3:60,8);
+gen_seidel_1e10_abs = gs_1e10(3:60,8);
+gen_seidel_eigen_abs = gs_eigen(3:60,8);
+gen_lu_eigen_abs = lu_eigen(3:60,8);
+gen_all_abs = [(gen_gauss_abs), (gen_gauss_sparse_abs), (gen_seidel_1e10_abs), (gen_seidel_eigen_abs), (gen_lu_eigen_abs)];
+
+plot(matrix_size, all_abs, "linewidth", 4);
+ylabel ("abs error");
+xlabel ("matrix size");
+title ("generate abs error");
+legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
+
+print -dpng ../../report/plots/05_gen_abs_error_all_methods.png;
+
+###gen all methods rel error
+gen_gauss_rel = gauss(3:60,8);
+gen_gauss_sparse_rel = gauss_sparse(3:60,8);
+gen_seidel_1e10_rel = gs_1e10(3:60,8);
+gen_seidel_eigen_rel = gs_eigen(3:60,8);
+gen_lu_eigen_rel = lu_eigen(3:60,8);
+gen_all_abs = [(gen_gauss_rel), (gen_gauss_sparse_rel), (gen_seidel_1e10_rel), (gen_seidel_eigen_rel), (gen_lu_eigen_rel)];
+
+plot(matrix_size, all_abs, "linewidth", 4);
+ylabel ("rel error");
+xlabel ("matrix size");
+title ("generate rel error");
+legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
+
+print -dpng ../../report/plots/06_gen_rel_error_all_methods.png;
 
 
 
