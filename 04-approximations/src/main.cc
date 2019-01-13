81a11c325eebc5c31e14afabbab31ab6fda03220
Stanisław Grams <sgrams@fmdx.pl>
2019-01-13 14:54:00 +0100

    new main.cc

--- 04-approximations/src/main.cc
+++ 04-approximations/src/main.cc
@@ -24,6 +24,12 @@
 #define DEFAULT_MIN_AGENTS_COUNT       3
 #define DEFAULT_MAX_AGENTS_COUNT      60
 
+#define DEFAULT_G_POLYNOMIAL 3
+#define DEFAULT_G_SPARSE_POLYNOMIAL 2
+#define DEFAULT_GS_1E10_POLYNOMIAL 2
+#define DEFAULT_GS_EIGEN_POLYNOMIAL 2
+#define DEFAULT_LU_EIGEN_POLYNOMIAL 1
+
 using namespace std;
 typedef enum {
   G,
@@ -69,24 +75,33 @@ get_measurements (types_t type, int min_agents_count, int max_agents_count)
     measurements->generator_measurements[i] = new double[max_agents_count - min_agents_count + 1];
     measurements->calculation_measurements[i] = new double[max_agents_count - min_agents_count + 1];
   }
+
   for (int i = min_agents_count; i <= max_agents_count; ++i)
   {
     clock_t begin_generating_time = clock ();
     if (type == GS_EIGEN || type == LU_EIGEN) {
-      sg = new SparseGenerator<double>(i);
+      sg = new SparseGenerator<double>(i-1);
     } else {
-      g = new Generator (i);
+      g = new Generator (i-1);
     }
     clock_t end_generating_time = clock ();
 
     // save measurement to measurements structure
-    measurements->generator_measurements[0][i-min_agents_count] = i;
     measurements->generator_measurements[1][i-min_agents_count] = (double)(end_generating_time - begin_generating_time) / CLOCKS_PER_SEC;
     // transfer generated structured to MyMatrix/MySparseMatrix object (by pointers -> in almost no time)
     if (type == GS_EIGEN || type == LU_EIGEN) {
       sm = new MySparseMatrix<double> (sg->get_cases_count (), sg->get_matrix (), sg->get_matrix_vector ());
     } else {
       m = new MyMatrix<double> (g->get_cases_count (), g->get_matrix (), g->get_matrix_vector ());
+    }
+
+    // save matrix size to the data structure
+    if (type == GS_EIGEN || type == LU_EIGEN) {
+      measurements->calculation_measurements[0][i-min_agents_count] = sg->get_cases_count ();
+      measurements->generator_measurements[0][i-min_agents_count] = sg->get_cases_count ();
+    } else {
+      measurements->calculation_measurements[0][i-min_agents_count] = g->get_cases_count ();
+      measurements->generator_measurements[0][i-min_agents_count] = g->get_cases_count ();
     }
 
     // begin calculations
@@ -121,8 +136,11 @@ get_measurements (types_t type, int min_agents_count, int max_agents_count)
         end_calculation_time = clock ();
         break;
     }
-    measurements->calculation_measurements[0][i-min_agents_count] = i;
+
+    // save result to the data structure
     measurements->calculation_measurements[1][i-min_agents_count] = (double)(end_calculation_time - begin_calculation_time) / CLOCKS_PER_SEC;
+
+    // clear and free memory
     if (type == GS_EIGEN || type == LU_EIGEN) {
       delete sg;
       delete sm;
@@ -138,10 +156,16 @@ get_measurements (types_t type, int min_agents_count, int max_agents_count)
 void
 perform_calculations ()
 {
+  int size = DEFAULT_MAX_AGENTS_COUNT - DEFAULT_MIN_AGENTS_COUNT + 1;
   Approximation<double> *ap;
-  measurement_t *gaussian = get_measurements (G, DEFAULT_MIN_AGENTS_COUNT, 60);
-  ap = new Approximation<double>(gaussian->calculation_measurements[0], gaussian->calculation_measurements[1], 60-DEFAULT_MIN_AGENTS_COUNT + 1, 3);
-  double *results = ap->run ();
+  measurement_t *measure;
+  double *results;
+
+/*
+  // GAUSSIAN PARTIAL
+  measure = get_measurements (G, DEFAULT_MIN_AGENTS_COUNT, DEFAULT_MAX_AGENTS_COUNT);
+  ap = new Approximation<double>(measure->calculation_measurements[0], measure->calculation_measurements[1], size, DEFAULT_G_POLYNOMIAL);
+  results = ap->run ();
 
   for (int i = 0; i < 3; ++i)
   {
@@ -149,7 +173,47 @@ perform_calculations ()
   }
   cout << endl;
 
-  delete_measurements (gaussian, DEFAULT_MIN_AGENTS_COUNT, 5);
+  delete_measurements (measure, DEFAULT_MIN_AGENTS_COUNT, DEFAULT_MAX_AGENTS_COUNT);
+  delete[] results;
+  delete ap;
+*/
+
+  // GAUSSIAN PARTIAL FOR SPARSE MATRICES
+  measure = get_measurements (G_SPARSE, DEFAULT_MIN_AGENTS_COUNT, DEFAULT_MAX_AGENTS_COUNT);
+  ap = new Approximation<double>(measure->calculation_measurements[0], measure->calculation_measurements[1], size, DEFAULT_G_SPARSE_POLYNOMIAL);
+  results = ap->run ();
+
+  for (int i = 0; i < DEFAULT_G_SPARSE_POLYNOMIAL + 1; ++i)
+  {
+    cout << results[i] << " * x^" << i;
+    if (i + 1 < DEFAULT_G_SPARSE_POLYNOMIAL + 1)
+      cout << " + ";
+  }
+  cout << endl;
+  delete_measurements (measure, DEFAULT_MIN_AGENTS_COUNT, DEFAULT_MAX_AGENTS_COUNT);
+  delete[] results;
+  delete ap;
+
+  // GAUSSIAN PARTIAL FOR SPARSE MATRICES
+  measure = get_measurements (G_SPARSE, DEFAULT_MIN_AGENTS_COUNT, DEFAULT_MAX_AGENTS_COUNT);
+  ap = new Approximation<double>(measure->calculation_measurements[0], measure->calculation_measurements[1], size, DEFAULT_G_SPARSE_POLYNOMIAL);
+  results = ap->run ();
+
+  for (int i = 0; i < DEFAULT_G_SPARSE_POLYNOMIAL + 1; ++i)
+  {
+    cout << results[i] << " * x^" << i;
+    if (i + 1 < DEFAULT_G_SPARSE_POLYNOMIAL + 1)
+      cout << " + ";
+  }
+
+  for (int i = 0; i < size; ++i)
+  {
+    cout << "[" << measure->calculation_measurements[0][i] << "]: " << measure->calculation_measurements[1][i] << endl;
+  }
+
+  cout << endl;
+  delete_measurements (measure, DEFAULT_MIN_AGENTS_COUNT, DEFAULT_MAX_AGENTS_COUNT);
+  delete[] results;
   delete ap;
 }
 
