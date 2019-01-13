94716abce73acad6ae85a9c6196d877dd3322f9d
Stanisław Grams <sgrams@fmdx.pl>
2019-01-13 11:44:40 +0100

    fixed gauss-seidel

--- 04-approximations/src/sparsematrix.hh
+++ 04-approximations/src/sparsematrix.hh
@@ -30,14 +30,24 @@ class MySparseMatrix {
       this->width  = width;
     }
     
-    void copy_vector (Eigen::SparseVector<T> to_vector, Eigen::SparseVector<T> from_vector, int width)
+    void
+    copy_vector (Eigen::SparseVector<T> *to_vector, Eigen::SparseVector<T> *from_vector, int width)
     {
       for (int i = 0; i < width; ++i)
       {
-        if (from_vector.coeffRef(i) != 0) {
-          to_vector.insert (i) = from_vector.coeffRef (i);
+        if (from_vector->coeffRef (i) != 0) {
+          to_vector->coeffRef (i) = from_vector->coeffRef (i);
         }
       }
+    }
+
+    double
+    vector_norm (Eigen::SparseVector<T> *ret_vec, Eigen::SparseVector<T> *tmp_vec, int width)
+    {
+      double norm_1 = (double) ret_vec->squaredNorm ();
+      double norm_2 = (double) tmp_vec->squaredNorm ();
+
+      return (double) fabs (norm_1 - norm_2);
     }
 
     Eigen::VectorXd
@@ -51,6 +61,43 @@ class MySparseMatrix {
       solver.analyzePattern (*this->matrix);
       solver.factorize (*this->matrix);
       ret_vec = solver.solve (*this->vector);
+
+      return ret_vec;
+    }
+
+    Eigen::VectorXd
+    sparse_GS_approx (double eps)
+    {
+      Eigen::SparseMatrix<T> *A = this->matrix;
+      Eigen::SparseVector<T> *b = this->vector;
+
+      Eigen::SparseVector<T> ret_vec (this->width);
+      Eigen::SparseVector<T> pre_vec (this->width);
+
+      double result;
+      A->makeCompressed ();
+
+      do {
+        for (int i = 0; i < this->width; ++i)
+        {
+          ret_vec.coeffRef (i) = b->coeffRef (i);
+          for (int j = 0; j < i; ++j)
+          {
+            ret_vec.coeffRef (i) -= (pre_vec.coeffRef(j) * A->coeffRef(i, j));
+          }          
+          for (int j = i + 1; j < this->width; ++j)
+          {
+            ret_vec.coeffRef (i) -= (pre_vec.coeffRef(j) * A->coeffRef(i, j));
+          }
+          if (ret_vec.coeffRef (i) != 0) {
+            ret_vec.coeffRef (i) /= A->coeffRef (i, i);
+          }
+        }
+
+
+        result = vector_norm (&ret_vec, &pre_vec, this->width);
+        copy_vector (&pre_vec, &ret_vec, this->width);
+      } while (result > eps);
 
       return ret_vec;
     }
@@ -70,22 +117,22 @@ class MySparseMatrix {
       do {
         for (int i = 0; i < this->width; ++i)
         {
-          if (b->coeffRef (i) != 0) {
-            ret_vec.insert (i) = b->coeffRef (i);
-          }
+          ret_vec.coeffRef (i) = b->coeffRef (i);
           for (int j = 0; j < i; ++j)
           {
-            ret_vec.coeffRef (i) = ret_vec.coeffRef (i) - (pre_vec.coeffRef(j) * A->coeffRef(i, j));
+            ret_vec.coeffRef (i) -= (pre_vec.coeffRef(j) * A->coeffRef(i, j));
           }          
           for (int j = i + 1; j < this->width; ++j)
           {
-            ret_vec.coeffRef (i) = ret_vec.coeffRef (i) - (pre_vec.coeffRef(j) * A->coeffRef(i, j));
-          }  
-          ret_vec.coeffRef (i) = ret_vec.coeffRef (i) / A->coeffRef (i, i);
+            ret_vec.coeffRef (i) -= (pre_vec.coeffRef(j) * A->coeffRef(i, j));
+          }
+          if (ret_vec.coeffRef (i) != 0) {
+            ret_vec.coeffRef (i) /= A->coeffRef (i, i);
+          }
         }
 
         iterator--;
-        pre_vec = ret_vec;
+        copy_vector (&pre_vec, &ret_vec, this->width);
       } while (iterator > 0);
 
       return ret_vec;
