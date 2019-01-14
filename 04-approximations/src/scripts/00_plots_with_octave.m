24352d5021b887cd379e75f145cc5722b08f8381
Juliusz <gaunt1111@gmail.com>
2019-01-14 10:10:49 +0100

    fixes

--- 04-approximations/src/scripts/00_plots_with_octave.m
+++ 04-approximations/src/scripts/00_plots_with_octave.m
@@ -43,67 +43,63 @@ legend ("G", "G\\_SPARSE    ", "GS\\_1E-10", "GS\\_EIGEN", "LU\\_EIGEN", "locati
 
 print -dpng ../../report/plots/02_gen_time_execution_all_methods.png;
 
-###calc all methods abs error
-gauss_abs = gauss(3:60,4);
+###calc sparse abs error
 gauss_sparse_abs = gauss_sparse(3:60,4);
-seidel_1e10_abs = gs_1e10(3:60,4);
 seidel_eigen_abs = gs_eigen(3:60,4);
 lu_eigen_abs = lu_eigen(3:60,4);
-all_abs = [(gauss_abs), (gauss_sparse_abs), (seidel_1e10_abs), (seidel_eigen_abs), (lu_eigen_abs)];
+sparse_abs = [(gauss_sparse_abs), (seidel_eigen_abs), (lu_eigen_abs)];
 
-plot(matrix_size, all_abs, "linewidth", 4);
+plot(matrix_size, sparse_abs, "linewidth", 4);
 ylabel ("abs error");
 xlabel ("matrix size");
-title ("calculation abs error");
-legend ("G", "G\\_SPARSE    ", "GS\\_1E-10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
+title ("calculation sparse methods abs error");
+legend ("G\\_SPARSE    ", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
 
-print -dpng ../../report/plots/03_calc_abs_error_all_methods.png;
+print -dpng ../../report/plots/03_calc_abs_error_sparse_methods.png;
 
-% ###calc all methods rel error
-% gauss_rel = gauss(3:60,5);
-% gauss_sparse_rel = gauss_sparse(3:60,5);
-% seidel_1e10_rel = gs_1e10(3:60,5);
-% seidel_eigen_rel = gs_eigen(3:60,5);
-% lu_eigen_rel = lu_eigen(3:60,5);
-% all_rel = [(gauss_rel), (gauss_sparse_rel), (seidel_1e10_rel), (seidel_eigen_rel), (lu_eigen_rel)];
+###calc norm abs error
+gauss_abs = gauss(3:60,4);
+seidel_1e10_abs = gs_1e10(3:60,4);
+lu_eigen_abs = lu_eigen(3:60,4);
+stanard_abs = [(gauss_abs), (seidel_1e10_abs)];
 
-% plot(matrix_size, all_rel, "linewidth", 4);
-% ylabel ("rel error");
-% xlabel ("matrix size");
-% title ("calculation rel error");
-% legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
+plot(matrix_size, stanard_abs, "linewidth", 4);
+ylabel ("abs error");
+xlabel ("matrix size");
+title ("calculation standard methods abs error");
+legend ("G  ","GS\\_1E-10   ", "location", "northwest");
 
-###gen all methods abs error
-gen_gauss_abs = gauss(3:60,8);
+print -dpng ../../report/plots/04_calc_abs_error_standard_methods.png;
+
+
+###gen sparse abs error
 gen_gauss_sparse_abs = gauss_sparse(3:60,8);
-gen_seidel_1e10_abs = gs_1e10(3:60,8);
 gen_seidel_eigen_abs = gs_eigen(3:60,8);
 gen_lu_eigen_abs = lu_eigen(3:60,8);
-gen_all_abs = [(gen_gauss_abs), (gen_gauss_sparse_abs), (gen_seidel_1e10_abs), (gen_seidel_eigen_abs), (gen_lu_eigen_abs)];
+gen_sparse_abs = [(gen_gauss_sparse_abs), (gen_seidel_eigen_abs), (gen_lu_eigen_abs)];
 
-plot(matrix_size, gen_all_abs, "linewidth", 4);
+plot(matrix_size, gen_sparse_abs, "linewidth", 4);
 ylabel ("abs error");
 xlabel ("matrix size");
-title ("generate abs error");
-legend ("G", "G\\_SPARSE    ", "GS\\_1E-10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
+title ("generate sparse methods abs error");
+legend ("G\\_SPARSE    ", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
 
-print -dpng ../../report/plots/05_gen_abs_error_all_methods.png;
+print -dpng ../../report/plots/05_gen_abs_error_sparse_methods.png;
 
-% ###gen all methods rel error
-% gen_gauss_rel = gauss(3:60,8);
-% gen_gauss_sparse_rel = gauss_sparse(3:60,8);
-% gen_seidel_1e10_rel = gs_1e10(3:60,8);
-% gen_seidel_eigen_rel = gs_eigen(3:60,8);
-% gen_lu_eigen_rel = lu_eigen(3:60,8);
-% gen_all_abs = [(gen_gauss_rel), (gen_gauss_sparse_rel), (gen_seidel_1e10_rel), (gen_seidel_eigen_rel), (gen_lu_eigen_rel)];
+###gen standard abs error
+gen_gauss_abs = gauss(3:60,8);
+gen_seidel_1e10_abs = gs_1e10(3:60,8);
+gen_standard_abs = [(gen_gauss_abs), (gen_seidel_1e10_abs)];
 
-% plot(matrix_size, all_abs, "linewidth", 4);
-% ylabel ("rel error");
-% xlabel ("matrix size");
-% title ("generate rel error");
-% legend ("G", "G\\_SPARSE    ", "GS\\_1E10", "GS\\_EIGEN", "LU\\_EIGEN", "location", "northwest");
+plot(matrix_size, gen_standard_abs, "linewidth", 4);
+ylabel ("abs error");
+xlabel ("matrix size");
+title ("generate standard methods abs error");
+legend ("G", "GS\\_1E-10    ", "location", "northwest");
 
-% print -dpng ../../report/plots/06_gen_rel_error_all_methods.png;
+print -dpng ../../report/plots/06_gen_abs_error_standard_methods.png;
+
+
 
 
 
