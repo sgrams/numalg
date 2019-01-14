b1b185b583739e3dea6693022da89f3c588db0cc
Juliusz <gaunt1111@gmail.com>
2019-01-14 21:53:06 +0100

    [04-approximations] final script and final plots

--- 04-approximations/src/scripts/00_plots_with_octave.m
+++ 04-approximations/src/scripts/00_plots_with_octave.m
@@ -2,10 +2,22 @@ graphics_toolkit ("gnuplot");
 format long;
 output_precision = 34;
 load ../../csv/gauss.csv;
+load ../../csv/gauss_plus.csv;
+load ../../csv/gauss_minus.csv;
 load ../../csv/gauss_sparse.csv
+load ../../csv/gauss_sparse_plus.csv
+load ../../csv/gauss_sparse_minus.csv
 load ../../csv/gs_1e10.csv
+load ../../csv/gs_1e10_plus.csv
+load ../../csv/gs_1e10_minus.csv
 load ../../csv/gs_eigen.csv
+load ../../csv/gs_eigen_plus.csv
+load ../../csv/gs_eigen_minus.csv
 load ../../csv/lu_eigen.csv
+load ../../csv/lu_eigen_plus.csv
+
+
+
 
 
 ###all methods calc execution time
@@ -98,6 +110,157 @@ title ("generate standard methods abs error");
 legend ("G", "GS\\_1E-10    ", "location", "northwest");
 
 print -dpng ../../report/plots/06_gen_abs_error_standard_methods.png;
+
+##########################################plus######################minus
+
+###calc gauss plus minus
+gauss_time = gauss(3:60,3);
+gauss_approx = gauss(3:60,2);
+gauss_plus_approx = gauss_plus(3:60,2);
+gauss_minus_approx = gauss_minus(3:60,2);
+gauss_all = [(gauss_time), (gauss_approx), (gauss_plus_approx), (gauss_minus_approx)];
+
+plot(matrix_size, gauss_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("CALC GAUSS");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/07_calc_gauss_plus_minus_time.png;
+
+###gen gauss plus minus
+gen_gauss_time = gauss(3:60,7);
+gen_gauss_approx = gauss(3:60,6);
+gen_gauss_plus_approx = gauss_plus(3:60,6);
+gen_gauss_minus_approx = gauss_minus(3:60,6);
+gen_gauss_all = [(gen_gauss_time), (gen_gauss_approx), (gen_gauss_plus_approx), (gen_gauss_minus_approx)];
+
+plot(matrix_size, gen_gauss_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("GEN GAUSS");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/08_gen_gauss_plus_minus_time.png;
+
+###calc gauss sparse plus minus
+gauss_sparse_time = gauss_sparse(3:60,3);
+gauss_sparse_approx = gauss_sparse(3:60,2);
+gauss_sparse_plus_approx = gauss_sparse_plus(3:60,2);
+gauss_sparse_minus_approx = gauss_sparse_minus(3:60,2);
+gauss_sparse_all = [(gauss_sparse_time), (gauss_sparse_approx), (gauss_sparse_plus_approx), (gauss_sparse_minus_approx)];
+
+plot(matrix_size, gauss_sparse_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("CALC G\\_SPARSE");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/09_calc_gauss_sparse_plus_minus_time.png;
+
+###gen gauss sparse plus minus
+gen_gauss_sparse_time = gauss_sparse(3:60,7);
+gen_gauss_sparse_approx = gauss_sparse(3:60,6);
+gen_gauss_sparse_plus_approx = gauss_sparse_plus(3:60,6);
+gen_gauss_sparse_minus_approx = gauss_sparse_minus(3:60,6);
+gen_gauss_sparse_all = [(gen_gauss_sparse_time), (gen_gauss_sparse_approx), (gen_gauss_sparse_plus_approx), (gen_gauss_sparse_minus_approx)];
+
+plot(matrix_size, gen_gauss_sparse_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("GEN G\\_SPARSE");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/10_gen_gauss_sparse_plus_minus_time.png;
+
+###calc gauss 1e10 plus minus
+gauss_1e10_time = gs_1e10(3:60,3);
+gauss_1e10_approx = gs_1e10(3:60,2);
+gauss_1e10_plus_approx = gs_1e10_plus(3:60,2);
+gauss_1e10_minus_approx = gs_1e10_minus(3:60,2);
+gauss_1e10_all = [(gauss_1e10_time), (gauss_1e10_approx), (gauss_1e10_plus_approx), (gauss_1e10_minus_approx)];
+
+plot(matrix_size, gauss_1e10_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("CALC GS\\_1E-10");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/11_calc_gauss_1e10_plus_minus_time.png;
+
+###calc gauss 1e10 plus minus
+gen_gauss_1e10_time = gs_1e10(3:60,7);
+gen_gauss_1e10_approx = gs_1e10(3:60,6);
+gen_gauss_1e10_plus_approx = gs_1e10_plus(3:60,6);
+gen_gauss_1e10_minus_approx = gs_1e10_minus(3:60,6);
+gen_gauss_1e10_all = [(gen_gauss_1e10_time), (gen_gauss_1e10_approx), (gen_gauss_1e10_plus_approx), (gen_gauss_1e10_minus_approx)];
+
+plot(matrix_size, gen_gauss_1e10_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("GEN GS\\_1E-10");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/12_calc_gauss_1e10_plus_minus_time.png;
+
+###calc gauss eigen plus minus
+gs_eigen_time = gs_eigen(3:60,3);
+gs_eigen_approx = gs_eigen(3:60,2);
+gs_eigen_plus_approx = gs_eigen_plus(3:60,2);
+gs_eigen_minus_approx = gs_eigen_minus(3:60,2);
+gs_eigen_all = [(gs_eigen_time), (gs_eigen_approx), (gs_eigen_plus_approx), (gs_eigen_minus_approx)];
+
+plot(matrix_size, gs_eigen_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("CALC GS\\_EIGEN");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/13_calc_gauss_eigen_plus_minus_time.png;
+
+###gen gauss eigen plus minus
+gen_gs_eigen_time = gs_eigen(3:60,7);
+gen_gs_eigen_approx = gs_eigen(3:60,6);
+gen_gs_eigen_plus_approx = gs_eigen_plus(3:60,6);
+gen_gs_eigen_minus_approx = gs_eigen_minus(3:60,6);
+gen_gs_eigen_all = [(gen_gs_eigen_time), (gen_gs_eigen_approx), (gen_gs_eigen_plus_approx), (gen_gs_eigen_minus_approx)];
+
+plot(matrix_size, gen_gs_eigen_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("GEN GS\\_EIGEN");
+legend("real\\_time", "approximation", "approximation (+1)", "approximation (-1)", "location", "northwest");
+
+print -dpng ../../report/plots/14_gen_gauss_eigen_plus_minus_time.png;
+
+###calc lu eigen plus minus
+lu_eigen_time = lu_eigen(3:60,3);
+lu_eigen_approx = lu_eigen(3:60,2);
+lu_eigen_plus_approx = lu_eigen_plus(3:60,2);
+lu_eigen_all = [(lu_eigen_time), (lu_eigen_approx), (lu_eigen_plus_approx)];
+
+plot(matrix_size, lu_eigen_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("CALC LU\\_EIGEN");
+legend("real\\_time", "approximation", "approximation (+1)", "location", "northwest");
+
+print -dpng ../../report/plots/15_calc_lu_eigen_plus_minus_time.png;
+
+###gen lu eigen plus minus
+gen_lu_eigen_time = lu_eigen(3:60,7);
+gen_lu_eigen_approx = lu_eigen(3:60,6);
+gen_lu_eigen_plus_approx = lu_eigen_plus(3:60,6);
+gen_lu_eigen_all = [(gen_lu_eigen_time), (gen_lu_eigen_approx), (gen_lu_eigen_plus_approx)];
+
+plot(matrix_size, gen_lu_eigen_all, "linewidth", 4);
+ylabel ("time");
+xlabel ("matrix size");
+title ("GEN LU\\_EIGEN");
+legend("real\\_time", "approximation", "approximation (+1)", "location", "northwest");
+
+print -dpng ../../report/plots/16_gen_lu_eigen_plus_minus_time.png;
+
 
 
 
